723c5c952ca66870e199767f6669cc618b2e4fcd
liu.bian.shi <liu.bian.shi@gmail.com>
2023-05-10 08:54:09 +0800

    代码格式调整

--- parse_rmd.R
+++ parse_rmd.R
@@ -42,6 +42,20 @@ complete_config_info <- function(config, default) {
     })
 }
 
+get_meta <- function(outformat, infile, yaml_file, ...) {
+    default_meta <- list(
+        infile            = infile,
+        outfile           = sub("\\.[Rr](md|markdown)$", ".knit.md", infile),
+        render            = "rmarkdown::render",
+        run_pandoc        = FALSE,
+        opt               = list(latex_engine     = "xelatex",
+                                clean_supporting = FALSE),
+        intermediates_dir = tempdir(),
+        output_dir        = tempdir()
+    )
+    config_meta <- yaml::read_yaml(yaml_file)[[outformat]]
+    complete_config_info(config_meta, default_meta)
+}
 write_knit_meta <- function(pares_res, intermediates_dir) {
     knit_meta <- attr(parse_res, 'knit_meta')
     if (is.null(knit_meta)) return(NULL)
@@ -62,38 +76,27 @@ write_knit_meta <- function(pares_res, intermediates_dir) {
     return(outfile)
 }
 
-args <- parse_args()
-default_meta <- list(
-    infile            = args$infile,
-    outfile           = sub("\\.[Rr](md|markdown)$", ".knit.md", args$infile),
-    render            = "rmarkdown::render",
-    run_pandoc        = FALSE,
-    opt               = list(latex_engine     = "xelatex",
-                             clean_supporting = FALSE),
-    intermediates_dir = tempdir(),
-    output_dir        = tempdir()
-)
-meta <- yaml::read_yaml(args$yaml_file)[[args$outformat]] |>
-        complete_config_info(default_meta)
-
-diy_output_format <- rlang::exec(rlang::parse_expr(meta$out), !!!(meta$opt))
-parse_res <- rlang::exec(rlang::parse_expr(meta$render),
-                         output_format     = diy_output_format,
-                         run_pandoc        = meta$run_pandoc,
-                         output_dir        = meta$output_dir,
-                         intermediates_dir = meta$intermediates_dir)
+args          <- parse_args()
+meta          <- do.call(get_meta, args)
+output_format <- rlang::exec(rlang::parse_expr(meta$out), !!!(meta$opt))
+parse_res     <- rlang::exec(rlang::parse_expr(meta$render),
+                             output_format     = output_format,
+                             run_pandoc        = meta$run_pandoc,
+                             output_dir        = meta$output_dir,
+                             intermediates_dir = meta$intermediates_dir)
 
 if (!is.null(meta$superfluous_dir)) {
     meta$output_dir <- file.path(meta$superfluous_dir, meta$output_dir)
 }
-
 meta$outfile     <- file.path(meta$output_dir, meta$outfile)
 meta$knit_meta   <- write_knit_meta(parse_res, meta$output_dir)
 meta$lua_filters <- get_pandoc_lua_filter()
 
 if (is.null(args$result_file)) {
-    args$result_file <- file.path(meta$intermediates_dir, "rmd_meta.yaml")
+    args$result_file <- file.path(meta$output_dir, "rmd_meta.yaml")
 }
+
 yaml::write_yaml(meta, args$result_file)
 
 cat("Rmarkdown Parse Success\n")
+# END
